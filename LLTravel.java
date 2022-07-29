ec0a39ad52fb68b6688de3d32ca884c81e18a54b
chipademayur <101926125+chipademayur@users.noreply.github.com>
2022-07-29 23:50:52 +0530

    Add files via upload

---
+++ LLTravel.java
@@ -0,0 +1,34 @@
+package middle;
+
+public class LLTravel {
+	static void printLL(Node head) {
+		Node cur=head;
+		while(cur !=null) {
+			System.out.print(cur.data+" -> ");
+			cur=cur.next;
+		}
+		System.out.print("end");
+		}
+
+	public static void main(String[] args) {
+		Node n1=new Node(10);
+		Node n2=new Node(20);
+		Node n3=new Node(30);
+		Node n4=new Node(40);
+		
+		Node head=n1;
+		n1.next=n2;
+		n2.next=n3;
+		n3.next=n4;
+		printLL(head);
+
+	}
+
+}
+class Node{
+	int data;
+	Node next;
+	public Node(int data) {
+		this.data=data;
+	}
+}
